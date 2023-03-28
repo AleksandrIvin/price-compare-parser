2cfccb758ce8906989a94152518d74909443bd95
Alex <aleksandr.ivin.ua@gmail.com>
2023-03-28 18:17:50 +0200

    Checking again

--- www/index.php
+++ www/index.php
@@ -3,7 +3,8 @@ $params = array_merge(
     require(__DIR__ . '/config/params.php'),
     require(__DIR__ . '/config/params-local.php')
 );
-echo 'It works from FTP github actions!';
+echo 'This code has been deployed by the FTP github action! <br/>';
+echo 'These params have been ovewritten by params-local.php: <br/>';
 echo $params['adminEmail'];
 
 ?>
