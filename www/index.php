02fe49c78177dee707da1c58045edbcccd5fbc33
Alex <aleksandr.ivin.ua@gmail.com>
2023-03-28 17:50:51 +0200

    checking secret files

--- www/index.php
+++ www/index.php
@@ -1,6 +1,10 @@
 <?php
-
+$params = array_merge(
+    require(__DIR__ . '/config/params.php'),
+    require(__DIR__ . '/config/params-local.php')
+);
 echo 'It works from FTP github actions!';
+echo $params;
 
 ?>
 
