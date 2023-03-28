4e936deb2e77ca102b70ca3ca312a48d74163bc3
Alex <aleksandr.ivin.ua@gmail.com>
2023-03-28 20:11:34 +0200

    github.base_ref

--- www/config/params.php
+++ www/config/params.php
@@ -3,5 +3,6 @@
 return [
     'adminEmail' => '{{ SECRET_EMAIL }}',
     'adminName' => '{{ ADMIN_NAME }}',
+    'context' => '{{ CONTEXT_VAR }}',
 ];
 
--- www/index.php
+++ www/index.php
@@ -5,8 +5,12 @@ $params = array_merge(
 );
 echo 'This code has been deployed by the FTP github action! <br/>';
 echo 'These params have been ovewritten by params-local.php: <br/>';
+echo 'Github secret: ';
 echo $params['adminEmail'];
+echo 'Github variable: ';
 echo $params['adminName'];
+echo 'github context variable: ';
+echo $params['context'];
 
 ?>
 
